bda092901fef8f38fdbe72835928e5e7c6c9e5be
★LittlE_HearTs★ <148408585+littleheartsbms@users.noreply.github.com>
2025-03-17 23:59:06 +0700

    something for the home screen ig

--- torture/login.js
+++ torture/login.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var form = document.querySelector("input");
+    var form = document.querySelector("form");
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Prevent the default form submission
         window.location.href = "./home.html";
